81693b47b42e076520679f439ad11a225f301946
Hakim Assim <hakimassim2015@gmail.com>
2023-02-28 18:48:20 +0100

    new changes

--- src/components/helpers/inpute.js
+++ src/components/helpers/inpute.js
@@ -1,23 +1,30 @@
 export default function custome_inpute(props){
 
-    const inpute_css = {
-        padding: '12px 12px', 
-        borderRadius: '6px', 
-        borderWidth: '1px', 
-        marginTop:'6px', 
-        borderColor:'var(--gray)',
-        gridColumn:'1/3',
-        gridRow:'2/3'
-    }
 
-    const inpute_click = () => document.getElementById(props.label).removeAttribute('required');
+    const inpute_css = {padding: '12px 12px', borderRadius: '6px', marginTop:'6px', gridColumn:'1/3', gridRow:'2/3'}
+
+    const valide_inpute = {...inpute_css, ...{border:'1px solid var(--gray)'}}
+    const invalid_inpute = {...inpute_css, ...{border:'1px solid red'}}
     
 
     return (
+
+
         <div style={{display:'grid', gridTemplateColumns:'auto max-content'}}>
             <label className="label" htmlFor={props.label} style={{fontSize: '14px', color:'var(--marine-blue)'}}>{props.label}</label>
-            <input onClick={inpute_click} onChange={props.handle} id={props.label} style={inpute_css} value={props.value} type="text" placeholder={props.placeholder} />
-            <span className="span" style={{fontSize:'13px', display:'none'}}>The field is required</span>
+            
+            <input 
+                onClick={props.inpute} 
+                onChange={props.handle} 
+                id={props.label} 
+                style={props.error ? invalid_inpute : valide_inpute} 
+                value={props.value} 
+                placeholder={props.placeholder} />
+
+            {props.error ? <span className="span" style={{fontSize:'13px', color:'red'}}>
+                {props.value.length === 0 ? 'The field is required' : `Invalide ${props.label}`}
+            </span> : ''}
+
         </div>
     )
 }
--- src/components/templates/InfoTemplate.js
+++ src/components/templates/InfoTemplate.js
@@ -11,23 +11,24 @@ const Template = forwardRef(({step, updatestep}, info_ref) =>
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
 
+  const [errors, setErrors] = useState({name:false, email:false, phone:false})
+
   const regex_email = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$');
   const regex_name = new RegExp('[a-zA-Z]{3,}')
   const regex_phone = new RegExp('^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$')
 
   useImperativeHandle(info_ref , () => ({
     next() {
-      if(
-        regex_name.test(name) &&
-        regex_email.test(email) &&
-        regex_phone.test(phone)
-        
-      ){
+
+      if(!regex_name.test(name)) setErrors({...errors, name:true})
+      else if(!regex_email.test(email)) setErrors({...errors, email:true})
+      else if(!regex_phone.test(phone)) setErrors({...errors, phone:true})
+
+      else {
         dispatch(register({name:name, email:email, phone:phone}))
         updatestep(step + 1)
       }
       
-      else alert('Invalide')
     }
   }))
 
@@ -36,9 +37,9 @@ const Template = forwardRef(({step, updatestep}, info_ref) =>
     return (
         <>
             <div style={{display: 'flex', flexDirection: 'column', gap: '15px'}}>
-                <CustomeInpute value={name} handle={(event) => setName(event.target.value)}  label="Name" placeholder="Personal name"/>
-                <CustomeInpute value={email} handle={(event) => setEmail(event.target.value)}  label="Email Address" placeholder="e.g exemple@ix.netcom.com"/>
-                <CustomeInpute value={phone} handle={(event) => setPhone(event.target.value)} label="Phone Number" placeholder="e.g + 1 234 567 890"/>
+                <CustomeInpute inpute={() => setErrors({...errors, name:false})} value={name} error={errors.name} handle={(event) => setName(event.target.value)}  label="Name" placeholder="Personal name"/>
+                <CustomeInpute inpute={() => setErrors({...errors, email:false})} value={email} error={errors.email} handle={(event) => setEmail(event.target.value)}  label="Email Address" placeholder="e.g exemple@ix.netcom.com"/>
+                <CustomeInpute inpute={() => setErrors({...errors, phone:false})} value={phone} error={errors.phone} handle={(event) => setPhone(event.target.value)} label="Phone Number" placeholder="e.g + 1 234 567 890"/>
             </div>
         </>
     )
